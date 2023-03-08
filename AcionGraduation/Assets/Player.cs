39fcc953e1d95194203c1df42277c4a2acaed0e1
BlackPenguin-cpu <dkstmdwnswns@gmail.com>
2023-03-08 20:42:39 +0900

    Player Input

--- AcionGraduation/Assets/Player.cs
+++ AcionGraduation/Assets/Player.cs
@@ -1,16 +1,17 @@
+using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    public BoxCollider2D upperCutArea;
 
     [SerializeField]
     private float jumpPower;
     [SerializeField]
     private float spd;
 
-
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     private void Start()
     {
@@ -28,13 +29,17 @@ public class Player : MonoBehaviour
         {
             Jump();
         }
+        if (Input.GetKeyDown(KeyCode.J) && Input.GetKey(KeyCode.Space))
+        {
+            UpperCut();
+        }
     }
     private void Jump()
     {
         var ray = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, LayerMask.NameToLayer("Platform"));
         if (ray.transform != null) return;
 
-        rb.AddForce(Vector2.up * jumpPower);
+        rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
     }
     private void Move()
     {
@@ -42,5 +47,15 @@ public class Player : MonoBehaviour
         var spdValue = hor * spd * Time.deltaTime;
 
         transform.position = Vector3.right * spdValue;
+    }
+    private void UpperCut()
+    {
+        var ray = Physics2D.BoxCastAll(transform.position + (Vector3)upperCutArea.offset, upperCutArea.size, 0, Vector2.right, 0, LayerMask.NameToLayer("Enemy"));
+        foreach (RaycastHit2D physics2D in ray)
+        {
+            physics2D.transform.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+        rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+
     }
 }
